0320448b2d39d470f10eab2d1763eec1cff03a69
Dorin Popa <dorin.popa@ellation.com>
2020-03-04 11:34:55 +0200

    Initial commit

---
+++ src/components/EventRegistrationForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface Props {
+}
+
+export const EventRegistrationForm = (props: Props) => {
+  return (
+    <div>
+      Event Registration Form
+    </div>
+  )
+}
---
+++ src/containers/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import {EventRegistrationForm} from '../components/EventRegistrationForm'
+
+const App = () => {
+  return (
+    <div className="App">
+      <EventRegistrationForm />
+    </div>
+  );
+}
+
+export default App;
